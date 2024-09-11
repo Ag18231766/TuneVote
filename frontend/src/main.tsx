f45e9f2f51dccebad7fbd89329411ef4e705350d
Ag18231766 <amartyagupta18231766@gmail.com>
2024-09-11 08:19:35 +0530

    adding env variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-const apiUrl = import.meta.env.VITE_BACKEND_URL;
+const domain = import.meta.env.VITE_AUTH_0_DOMAIN_URL;
+const clientId = import.meta.env.VITE_AUTH_0_CLIENT_ID;
 const root = createRoot(document.getElementById('root')!);
 
 
 root.render(
 
 <Auth0Provider
-    domain="dev-deslwdyelw63ku4q.us.auth0.com"
-    clientId="HorCByRzeHJ6csLsCnnUUffqCVZT7w89"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: `http://localhost:5173/dashboard`,
       audience: 'this is an identifier',
