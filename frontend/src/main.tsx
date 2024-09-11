c47dbacdabc972dc2490339f2f4ade774bbec019
Ag18231766 <amartyagupta18231766@gmail.com>
2024-09-11 08:35:04 +0530

    added redirect url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 const domain = import.meta.env.VITE_AUTH_0_DOMAIN_URL;
 const clientId = import.meta.env.VITE_AUTH_0_CLIENT_ID;
+const redirectUrl = import.meta.env.VITE_REDIRECT_URI;
 const root = createRoot(document.getElementById('root')!);
 
 
@@ -12,7 +13,7 @@ root.render(
     domain={domain}
     clientId={clientId}
     authorizationParams={{
-      redirect_uri: `http://localhost:5173/dashboard`,
+      redirect_uri:redirectUrl,
       audience: 'this is an identifier',
       scope:'openid profile email'
     }}
